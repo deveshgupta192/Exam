ea3ecc93e1ce70e0ddfed7d3b4c4fbfa23b66eb4
eagaleye <eagaleye@cube9>
2018-03-12 18:32:34 +0530

    initial commit

---
+++ ds/src/main/java/com/bluebird/exam/ds/App.java
@@ -0,0 +1,13 @@
+package com.bluebird.exam.ds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
